a81239b1db09f06a3f1eca2abce4cc58123f3938
frostAdarsh <adarshmani017@gmail.com>
2024-06-19 20:19:54 +0530

    4th part is done

--- backend/controller.js/message.controller.js
+++ backend/controller.js/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.modal.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
@@ -28,8 +29,14 @@ export const sendMessage = async (req, res) => {
 
     // await conversation.save();
     // await newMessage.save();
-
     await Promise.all([conversation.save(), newMessage.save()]);
+
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      // io.to(<socket_id>).emit() used to send events to specific client
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
     res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in sendMessage controller", error.message);
@@ -45,9 +52,9 @@ export const getMessage = async (req, res) => {
     const conversation = await Conversation.findOne({
       participant: { $all: [senderId, userToChatId] },
     }).populate("message");
-    if(!conversation) return res.status(200).json([]);
+    if (!conversation) return res.status(200).json([]);
 
-    const message = conversation.message
+    const message = conversation.message;
 
     res.status(200).json(message);
   } catch (error) {
